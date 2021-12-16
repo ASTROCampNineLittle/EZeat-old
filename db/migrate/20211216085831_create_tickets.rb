dc292f4bba7d9d8562d3d300d0783e031857e9b2
Laichu <cozzichilli@gmail.com>
2021-12-16 17:18:18 +0800

    feat: plan and ticket model, migrate

--- db/migrate/20211216085831_create_tickets.rb
+++ db/migrate/20211216085831_create_tickets.rb
@@ -1,9 +1,9 @@
 class CreateTickets < ActiveRecord::Migration[6.1]
   def change
     create_table :tickets do |t|
-      t.string :time_start
-      t.string :time_end
-      t.string :date
+      t.time :time_start
+      t.time :time_end
+      t.date :date
       t.integer :quantity
       t.integer :plan_id
       t.iteger :store_id
